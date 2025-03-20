5512191378a896b30b4ab6286be43016479f271b
El Sayed Mahmoud <m.saijed@gmail.com>
2025-03-20 22:29:42 +0200

    st
    st

---
+++ lafu-category-checklist.php
@@ -0,0 +1,85 @@
+<?php
+/*
+Plugin Name: Parent Category AutoCheck + Category Tree Checklist
+Version: 1.2.0
+Description: Preserves the category hierarchy on the post editing screen + Check Parent Automatically + Auto scroll to first checked
+Author: Elsama
+Author URI: https://lafu.fi
+Plugin URI: https://lafu.fi/plugins/parent-category-autocheck
+License: GPLv2 or later (license.txt)
+Text Domain: cchl
+Domain Path: /languages
+*/
+
+define('CCHL_URL', plugin_dir_url(__FILE__));
+define('CCHL_PATH', plugin_dir_path(__FILE__));
+
+class Lafu_Category_Checklist {
+    public static function init() {
+        add_filter('wp_terms_checklist_args', [__CLASS__, 'modify_checklist_args']);
+        add_action('admin_footer', [__CLASS__, 'enqueue_scripts']);
+    }
+
+    public static function modify_checklist_args($args) {
+        $args['checked_ontop'] = false;
+        return $args;
+    }
+
+    public static function enqueue_scripts() {
+        if (!is_admin()) {
+            return;
+        }
+        ?>
+        <script>
+            (function($) {
+                function updateCategoryChecklist() {
+                    var categories = $('.categorychecklist, .cat-checklist');
+                    if (categories.length) {
+                        categories.each(function() {
+                            var firstChecked = $(this).find(':checkbox:checked').first();
+                            if (!firstChecked.length) return;
+                            var offset = firstChecked.position().top;
+                            $(this).closest('.tabs-panel').scrollTop(offset - 10);
+                        });
+                    }
+                }
+                $(document).ready(updateCategoryChecklist);
+            })(jQuery);
+        </script>
+        <?php
+    }
+}
+
+Lafu_Category_Checklist::init();
+
+add_action('admin_menu', 'cchl_admin_menu');
+function cchl_admin_menu() {
+    add_options_page('Category AutoCheck', 'Category AutoCheck', 'manage_options', 'category_autocheck', 'category_autocheck_options_page');
+}
+
+function category_autocheck_options_page() {
+    if (!current_user_can('manage_options')) {
+        wp_die(__('Unauthorized access.', 'cchl'));
+    }
+    ?>
+    <div class="wrap">
+        <h1><?php _e('Parent Category AutoCheck Settings', 'cchl'); ?></h1>
+        <form method="post" action="options.php">
+            <?php
+            settings_fields('cchl_settings_group');
+            do_settings_sections('category_autocheck');
+            submit_button();
+            ?>
+        </form>
+    </div>
+    <?php
+}
+
+add_action('admin_init', function() {
+    register_setting('cchl_settings_group', 'cchl_settings');
+    add_settings_section('cchl_main_section', __('Settings', 'cchl'), null, 'category_autocheck');
+    add_settings_field('enable_auto_check', __('Enable Auto Check', 'cchl'), function() {
+        $options = get_option('cchl_settings', ['enable_auto_check' => 1]);
+        echo '<input type="checkbox" name="cchl_settings[enable_auto_check]" value="1" ' . checked(1, $options['enable_auto_check'], false) . ' />';
+    }, 'category_autocheck', 'cchl_main_section');
+});
